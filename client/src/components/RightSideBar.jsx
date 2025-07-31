cfece7eb2aa9be40736df4f9cc21160dd9fb0c85
OMKAR KAVHALE <omkarkavhale44@gmail.com>
2025-07-31 22:21:57 +0530

    Initial commit: basic client side is created

---
+++ client/src/components/RightSideBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import assets from '../assets/assets'
+
+const RightSideBar = () => {
+  return (
+    <div>RightSideBar</div>
+  )
+}
+
+export default RightSideBar
---
+++ client/src/pages/ProfilePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfilePage = () => {
+  return (
+    <div>this is a profile page</div>
+  )
+}
+
+export default ProfilePage
